f8f32b21f3852c981eb2505630957493b658301f
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-04 22:10:50 -0300

    Add files via upload

---
+++ php8Exe01-mainResources/obrigado.php
@@ -0,0 +1,16 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Obrigado por não F# meu SYS ;D</title>
+</head>
+<body>
+    <h1>Obrigado tudo certo, POST recebido! ;D</h1>
+</body>
+</html>
